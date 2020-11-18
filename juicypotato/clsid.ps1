d303cd6f530265c92012a1d9ec571a6db0f221ab
shanfenglan <839735789@qq.com>
2020-11-18 10:51:53 +0800

    Create clsid.ps1

---
+++ juicypotato/clsid.ps1
@@ -0,0 +1,6 @@
+New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null
+$CLSID = Get-ItemProperty HKCR:\clsid\* | select-object AppID,@{N='CLSID'; E={$_.pschildname}} | where-object {$_.appid -ne $null}
+foreach($a in $CLSID)
+{
+	Write-Host $a.CLSID
+}
